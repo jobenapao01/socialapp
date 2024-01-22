7e144afb77e9da13fa2a70c507f020b4f86f3b9d
Joben Apao <jobenapao10@gmail.com>
2024-01-22 16:33:13 +0800

    like functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,21 @@
-import PostForm from "@/components/post-form"
-import Posts from "@/components/posts"
-import { fetchPosts } from "@/server/actions/create-post"
-import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from "@tanstack/react-query"
+import PostForm from '@/components/post-form';
+import Posts from '@/components/posts';
+import { fetchPosts } from '@/server/actions/create-post';
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
 
 export default async function Home() {
-  const queryClient = new QueryClient()
+	const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
-  })
-  return (
-    <main>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <PostForm />
-        <Posts />
-      </HydrationBoundary>
-    </main>
-  )
+	await queryClient.prefetchQuery({
+		queryKey: ['posts'],
+		queryFn: fetchPosts,
+	});
+	return (
+		<main>
+			<HydrationBoundary state={dehydrate(queryClient)}>
+				<PostForm />
+				<Posts />
+			</HydrationBoundary>
+		</main>
+	);
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,29 +1,33 @@
-import { Lobster_Two } from "next/font/google"
-import { auth } from "@/server/auth"
-import { redirect } from "next/navigation"
-import { Button } from "./ui/button"
-import { UserButton } from "./user-button"
-import Link from "next/link"
-const lobster = Lobster_Two({ subsets: ["latin"], weight: ["400"] })
+import { Mukta } from 'next/font/google';
+import { auth } from '@/server/auth';
+import { redirect } from 'next/navigation';
+import { Button } from './ui/button';
+import { UserButton } from './user-button';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+const mukta = Mukta({ subsets: ['latin'], weight: ['800'] });
 
 export default async function Nav() {
-  const user = await auth()
-  if (!user) redirect("/api/auth/signin")
-  return (
-    <nav className={lobster.className}>
-      <ul className="flex py-8 justify-between items-center">
-        <li>Post it</li>
+	const user = await auth();
+	if (!user) redirect('/api/auth/signin');
+	return (
+		<nav className={cn('text-2xl italic', mukta.className)}>
+			<ul className='flex py-8 justify-between items-center shadow-md'>
+				<li>Social App</li>
 
-        <li>
-          {!user ? (
-            <Button asChild>
-              <Link href={"/auth/login"}>Sign In</Link>
-            </Button>
-          ) : (
-            <UserButton expires={user.expires} user={user.user} />
-          )}
-        </li>
-      </ul>
-    </nav>
-  )
+				<li>
+					{!user ? (
+						<Button asChild>
+							<Link href={'/auth/login'}>Sign In</Link>
+						</Button>
+					) : (
+						<UserButton
+							expires={user.expires}
+							user={user.user}
+						/>
+					)}
+				</li>
+			</ul>
+		</nav>
+	);
 }
